78edf185257ec3e48aa89af1cbcfc23b1eda97c5
Radu <budeanuradu99@gmail.com>
2020-11-17 19:53:55 +0200

    basic implementation without UI

--- Receiver.py
+++ Receiver.py
@@ -7,7 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
 s.bind((IP, PORT))
 
 
-while True:
+for i in range(100):
 	data, address = s.recvfrom(1024) #Dimensiunea buffer-ului este de 1024 octeti
 										# Blocheaza thread-ul pana la primirea unui mesaj
 	print(f'Mesajul primit este {data} de la {address}')
--- Sender.py
+++ Sender.py
@@ -1,8 +1,13 @@
 import socket
+from packing_system import PackingSystem
 
 IP = "127.0.0.1"
 PORT = 1234
 msg = b"Hello from the other side."
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
-s.sendto(msg, (IP, PORT))
+
+ps = PackingSystem("Receiver.py")
+for i in range(100):
+	s.sendto(ps.pack_data(), (IP, PORT))
+ps.close_file()
