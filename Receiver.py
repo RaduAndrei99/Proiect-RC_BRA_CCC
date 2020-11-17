780734c1b5c78d0e1184dfb2d3a416d0e480462c
Cosmin29 <cosminccc28@gmail.com>
2020-11-17 19:38:56 +0200

    Implementarea Receiver-ului care permite despachetarea pachetelor primite si scrierea fisierului

--- Receiver.py
+++ Receiver.py
@@ -1,4 +1,7 @@
+import os
 import socket
+from FileWriter import FileWriter
+from UnPackingSystem import UnPackingSystem
 
 IP = "127.0.0.1"
 PORT = 1234
@@ -6,8 +9,38 @@ PORT = 1234
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
 s.bind((IP, PORT))
 
+# Dimeniunea fisierului va fi pe maxim 24 de octei -> maxim 320 MB
+# data[1:24] de la indexul 2 pana in indexul 24
+
+packet = SWPacket(24)
+fileWriter = FileWriter("")
+unPackingSystem = UnPackingSystem()
+
+name = ""
 
 while True:
-	data, address = s.recvfrom(1024) #Dimensiunea buffer-ului este de 1024 octeti
-										# Blocheaza thread-ul pana la primirea unui mesaj
-	print(f'Mesajul primit este {data} de la {address}')
+	dataReaded, address = s.recvfrom(24) # Dimensiunea buffer-ului este de 1024 24
+										 # Blocheaza thread-ul pana la primirea unui mesaj
+	
+	print(dataReaded)
+
+	packet.setData(dataReaded)
+	type, nrPacket, data = packet.unPack(packet);
+
+	if type == 1:
+		name += data.decode("ascii")
+
+	elif type == 0:
+		if fileWriter.isOpen() == False:
+			fileWriter.setFileName(name)
+			fileWriter.openFile()
+
+		if data == "":
+			break
+
+		fileWriter.writeInFile(data)
+
+fileWriter.closeFile()
+s.close()
+
+
---
+++ UnPackingSystem.py
@@ -0,0 +1,16 @@
+
+# Clasa permite despachetarea unui pachet primit
+
+class UnPackingSystem:
+
+	def __init__(self):
+		pass
+
+	def Unpack(self, packet):
+
+		type = int.from_bytes(packet.data[:1], "big")
+		nrPacket = int.from_bytes(packet.data[1:4], "big")
+		data = data[4:24]
+
+		return (type, nrPacket, data)
+
