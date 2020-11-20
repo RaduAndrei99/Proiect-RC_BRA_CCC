4e0b130b7c4c758528b35f214a6dfb3387e99920
Radu <budeanuradu99@gmail.com>
2020-11-20 10:57:04 +0200

    test

--- sender.py
+++ sender.py
@@ -9,14 +9,13 @@ packet_size = 36
 packet_data_size = 32
 packet_header_size = 4
 
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
 
 ps = PackingSystem()
-ps.open_file("dell.exe") # fisier de transmis, momentan hard-coded
+ps.open_file("receiver.py") # fisier de transmis, momentan hard-coded
 
 first_packet = SWPacket(packet_size, packet_data_size, packet_header_size, False)
-first_packet.store_data(b'dell.exe')
+first_packet.store_data(b'receiver.py')
 print(first_packet.get_data())
 s.sendto(first_packet.get_data(), (IP, PORT) )
 for i in range( int(ps.get_file_size() / ps.get_data_size_in_bytes() + 1)):
