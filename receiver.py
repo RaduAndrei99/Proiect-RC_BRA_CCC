e29db3d30aee2f32bc69c0e5f787bd9ccd4e5fde
Cosmin29 <cosminccc28@gmail.com>
2020-12-03 16:47:13 +0200

    Making receiver class

--- receiver.py
+++ receiver.py
@@ -1,50 +1,92 @@
 import os
 import socket
-from file_writer import FileWriter
+from __file_writer import FileWriter
 from unpacking_system import UnPackingSystem
 from packet import SWPacket
 
-IP = "127.0.0.1"
-PORT = 1234
+af_type_dic = {
+        	"AF_INET": socket.AF_INET,
+        	"AF_INET6": socket.AF_INET6,
+        	"AF_UNIX": socket.AF_UNIX
+}
 
-packet_size = 36
-packet_data_size = 32
-packet_header_size = 4
+sock_type_dic = {
+        "SOCK_STREAM": socket.SOCK_STREAM,
+        "SOCK_DGRAM": socket.SOCK_DGRAM
+}
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
-s.bind((IP, PORT))
+def check_socket(af_type, sock_type):
 
-# Dimeniunea fisierului va fi pe maxim 24 de octei -> maxim 320 MB
-# data[1:24] de la indexul 2 pana in indexul 24
+	try:
+		if af_type != "AF_INET" and af_type != "AF_INET6" and af_type != "AF_UNIX":
+			raise ValueError("Invalid protocol family type!")
 
-packet = SWPacket(packet_size,packet_data_size, packet_header_size, True)
-file_writer = FileWriter("") #fisier primit, momentan cu numele hard-coded
-ups = UnPackingSystem()
+		if sock_type != "SOCK_STREAM" and sock_type != "SOCK_DGRAM":
+			raise ValueError("Invalid socket type!")
+
+	except:
+		sys.exit(1)
+
+class Receiver:
+
+	DATA_PACKET_SIZE = 36
+	ACK_PACKET_SIZE = 4
+
+	DATA_SIZE = 32
+	PACKET_HEADER_SIZE = 4
+
+	def __init__(self, rcv_ip, rcv_port):
+		self.__receiver_ip = rcv_ip
+		self.__receiver_port = rcv_port
+
+		self.__file_writer = FileWriter("")
+		self.__ups = UnPackingSystem(DATA_PACKET_SIZE, DATA_SIZE)
+
+	def create_socket(self, af_type, sock_type):
+
+		check_socket(af_type, sock_type)
+
+		self.s = socket.socket(af_type_dic.get(af_type), sock_type_dic.get(sock_type)) # IPV4, UDP
+		self.s.bind((self.__receiver_ip, self.__receiver_port))
+
+	def start_receiver(self):
+		
+		packet = SWPacket(self.DATA_PACKET_SIZE, self.DATA_SIZE, self.PACKET_HEADER_SIZE, True)
+
+		name = "new_"
+
+		while True:
+			data_readed, address = self.s.recvfrom(self.DATA_PACKET_SIZE)
+
+			packet.create_packet(data_readed)
+			type, nr_packet, data = self.__ups.unpack(packet)
+
+			if type == 1:
+				name += data.decode("ascii")
+
+			elif type == 0:
+				if self.__file_writer.is_open() == False:
+					self.__file_writer.set_file_name(name)
+					self.__file_writer.open_file()
 
 
-name = "new_"
+				
 
-while True:
-	data_readed, address = s.recvfrom(packet_size) # Dimensiunea buffer-ului este de 1024 24
-										# Blocheaza thread-ul pana la primirea unui mesaj
-	
-	#print(data_readed)
+				#Threading
 
-	packet.create_packet(data_readed)
-	type, nr_packet, data = ups.unpack(packet)
+				self.__file_writer.write_in_file(data)
 
-	if type == 1:
-		name += data.decode("ascii")
-	elif type == 0:
-		if file_writer.is_open() == False:
-			file_writer.set_file_name(name)
-			file_writer.open_file()
+			else:
+				break
 
-		file_writer.write_in_file(data)
-	else:
-		break
+		self.__file_writer.close_file()
+		self.s.close()
 
-file_writer.close_file()
-s.close()
+
+receiver = Receiver("127.0.0.1", 1234)
+receiver.create_socket("AF_INET", "SOCK_DGRAM")
+receiver.start_receiver()
+
+
 
 
